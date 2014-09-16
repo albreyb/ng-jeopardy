3d94dab756731f8cb37adfaa5db6f14f8756b2f3
Albrey Brown <albrey.brown@gmail.com>
2014-09-16 16:47:09 -0700

    grid system created

---
+++ client/app/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('ng-jeopardy', []); // angular module created
+
+app.directive('jeopardyCard', function(){
+  return {
+    restrict: 'EAC',
+    link: function(scope, element, attributes){
+    },
+    templateUrl:'jeopardyCard.html',
+    replace: true,
+  };
+});
+
+app.controller('JeopardyCardController', function($scope){
+  $scope.data = [
+    {worth: 1000, question: "who's the weakest out", answer: "kia fathi"},
+    {worth: 1000, question: "who's the weakest out", answer: "kia fathi"},
+    {worth: 1000, question: "who's the weakest out", answer: "kia fathi"},
+    {worth: 1000, question: "who's the weakest out", answer: "kia fathi"},
+    {worth: 1000, question: "who's the weakest out", answer: "kia fathi"},
+
+  ];
+
+  $scope.title = ['Staff', 'JavaScript', 'San Francisco', 'FootBall', 'Hip Hop'];
+});
