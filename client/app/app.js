8ae37a9359b543839acfee67b393929091756eef
Mason <maseh87@gmail.com>
2014-09-17 16:45:58 -0700

    updated

--- client/app/app.js
+++ client/app/app.js
@@ -12,13 +12,20 @@ app.directive('jeopardyCard', function(){
 
 app.controller('JeopardyCardController', function($scope){
   $scope.data = [
-    {worth: 200, question: "who's the weakest out", answer: "kia fathi"},
-    {worth: 400, question: "who's the weakest out", answer: "kia fathi"},
-    {worth: 600, question: "who's the weakest out", answer: "kia fathi"},
-    {worth: 800, question: "who's the weakest out", answer: "kia fathi"},
-    {worth: 1000, question: "who's the weakest out", answer: "kia fathi"},
-
+    {worth: 200, question: "who's the weakest out", answer: "kia fathi", category: 'Staff'},
+    {worth: 400, question: "who's the weakest out", answer: "soldier boy", category: 'Hip Hop'},
+    {worth: 600, question: "who's the weakest out", answer: "kia fathi", category: 'FootBall'},
+    {worth: 800, question: "question dawg", answer: "kia fathi", category: 'Staff'},
+    {worth: 1000, question: "who's the weakest out", answer: "answer dawg", category: 'JavaScript'},
   ];
 
+  $scope.value = [200, 400, 600, 800, 1000];
+
   $scope.title = ['Staff', 'JavaScript', 'San Francisco', 'FootBall', 'Hip Hop'];
+
+  $scope.showAnswer = function(index, value) {
+    console.log($scope.title[index], value);
+  };
+
+
 });
