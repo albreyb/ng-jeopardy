a621664778b1638b659cc9a0c818b3ea09a41668
Albrey Brown <albrey.brown@gmail.com>
2014-09-17 16:47:40 -0700

    added alternative data collection method

--- client/app/app.js
+++ client/app/app.js
@@ -21,4 +21,186 @@ app.controller('JeopardyCardController', function($scope){
   ];
 
   $scope.title = ['Staff', 'JavaScript', 'San Francisco', 'FootBall', 'Hip Hop'];
+
+  $scope.allData = [
+    {
+      category: 'Staff Trivia',
+      questions: [
+          {
+            value: 200,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 400,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 600,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 800,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 1000,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          }
+        ],
+      },
+      {
+      category: 'JavaScript Battle',
+      questions: [
+          {
+            value: 200,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 400,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 600,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 800,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 1000,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          }
+        ],
+      },
+      {
+      category: 'Where In San Francisco',
+      questions: [
+          {
+            value: 200,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 400,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 600,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 800,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 1000,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          }
+        ],
+      },
+      {
+      category: "This and Other Gotchas",
+      questions: [
+          {
+            value: 200,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 400,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 600,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 800,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 1000,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          }
+        ],
+      },
+      {
+      category: "Name That Data Structure!",
+      questions: [
+          {
+            value: 200,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 400,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 600,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 800,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          },
+          {
+            value: 1000,
+            question: "whats your name",
+            answer: "albrey",
+            status: true,
+          }
+        ],
+      },
+  ];
+
+  $scope.showAnswer = function(){
+    console.log("hey")
+  }
 });
